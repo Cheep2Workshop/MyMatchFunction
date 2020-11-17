c4377f8ded562fd99dcc8da589351c68f2caf795
jack81722 <jack81722@hotmail.com.tw>
2020-11-17 16:44:05 +0800

    copied from tutorial 102

--- mmf/mymatchfunction.go
+++ mmf/mymatchfunction.go
@@ -10,18 +10,22 @@ import (
 )
 
 const (
-	matchName = "basic-matchfunction"
+	matchName              = "basic-matchfunction"
+	ticketsPerPoolPerMatch = 4
 )
 
+// Run is this match function's implementation of the gRPC call defined in api/matchfunction.proto.
 func (s *MatchFunctionService) Run(req *pb.RunRequest, stream pb.MatchFunction_RunServer) error {
+	// Fetch tickets for the pools specified in the Match Profile.
 	log.Printf("Generating proposals for function %v", req.GetProfile().GetName())
 
 	poolTickets, err := matchfunction.QueryPools(stream.Context(), s.queryServiceClient, req.GetProfile().GetPools())
 	if err != nil {
-		log.Printf("Failed to query tickets for the give pools, got %s", err.Error())
+		log.Printf("Failed to query tickets for the given pools, got %s", err.Error())
 		return err
 	}
 
+	// Generate proposals.
 	proposals, err := makeMatches(req.GetProfile(), poolTickets)
 	if err != nil {
 		log.Printf("Failed to generate matches, got %s", err.Error())
@@ -29,6 +33,7 @@ func (s *MatchFunctionService) Run(req *pb.RunRequest, stream pb.MatchFunction_R
 	}
 
 	log.Printf("Streaming %v proposals to Open Match", len(proposals))
+	// Stream the generated proposals back to Open Match.
 	for _, proposal := range proposals {
 		if err := stream.Send(&pb.RunResponse{Proposal: proposal}); err != nil {
 			log.Printf("Failed to stream proposals to Open Match, got %s", err.Error())
@@ -43,7 +48,6 @@ func makeMatches(p *pb.MatchProfile, poolTickets map[string][]*pb.Ticket) ([]*pb
 	var matches []*pb.Match
 	count := 0
 	for {
-		ticketsPerPoolPerMatch := 1
 		insufficientTickets := false
 		matchTickets := []*pb.Ticket{}
 		for pool, tickets := range poolTickets {
